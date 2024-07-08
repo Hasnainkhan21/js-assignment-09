e8a15f0fbbe255db44be6483d12e962aeaa35447
naino <Hasnainkhan21>
2024-07-08 16:47:42 +0500

    js-09

---
+++ index.js
@@ -0,0 +1,32 @@
+var number = +prompt("Enter a number")
+for (let i = 1; i<= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`)
+}
+
+        
+//  var array = []
+// for (let i = 0; i < 5; i++) {
+//     var number = +prompt("Enter a number");
+//     array.push[number]
+// }
+// let sum ;
+// for (let j = 0; j < array.length; j++) {
+//     sum += array[j];
+// }
+// console.log("the sum of array is " , sum); 
+
+// var array = []
+// for (let i = 0; i < 5; i++) {
+//     var number = +prompt("Enter a number");
+//     array.push[number]
+// }
+// let largest = array[0];
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] > largest) {
+//         largest = array[i];
+//     }
+// }
+// console.log("the largest number is ",largest);
+
+
+
